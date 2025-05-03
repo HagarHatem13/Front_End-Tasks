ea5f24b32c0e9f73be2f69f2541fb85b98f26737
HagarHatem13 <hagarhatem901@gmail.com>
2025-05-03 04:14:30 +0300

    Delete crudemailsjs directory

--- crudemailsjs/src/services/emailService.js
+++
@@ -1,9 +0,0 @@
-import emailjs from "emailjs-com";
-
-const SERVICE_ID = "service_4tw0ymo";
-const TEMPLATE_ID = "template_73l9dad";
-const USER_ID = "WosHSei2u1z3GiViw";
-
-export const sendEmail = (templateParams) => {
-  return emailjs.send(SERVICE_ID, TEMPLATE_ID, templateParams, USER_ID);
-};
